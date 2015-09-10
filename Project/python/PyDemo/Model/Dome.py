2c0fb18c075b5f99539a85dc2c1ab35883c6f492
RayleighChen <rayleighcl@gmail.com>
2015-09-10 21:29:51 +0800

    pyDome

---
+++ Project/python/PyDemo/Model/Dome.py
@@ -0,0 +1,14 @@
+    # -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 10 09:15:43 2015
+
+@author: Rayleigh
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(-8, 8, 1);
+y = np.cos(x);
+plt.plot(x, y);
+plt.show();
