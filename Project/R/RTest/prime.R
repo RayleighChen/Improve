6fc59ebf238d4b86f3e238e77764301806e97e52
RayleighChen <rayleighcl@gmail.com>
2015-09-11 22:06:09 +0800

    Rtest

---
+++ Project/R/RTest/prime.R
@@ -0,0 +1,12 @@
+# TODO: Add comment
+# 
+# Author: Rayleigh
+###############################################################################
+
+#y <- c(1, 3, 8, 23, 55)
+#x <- c(4, 5, 7, 15, 16)
+#plot(x, y)
+#abline(lm(y~x))
+
+dc <- read.csv(file.path("E:/workstation/新世界/申请书/data.csv"), header = TRUE)
+plot(dc)
