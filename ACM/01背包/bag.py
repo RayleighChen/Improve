f95d57c4ade7f5489ad4e9f0eed478a7eefef348
Rayleigh <rayleighcl@gmail.com>
2015-12-24 23:08:14 +0800

    Update bag.py

--- ACM/01背包/bag.py
+++ ACM/01背包/bag.py
@@ -12,17 +12,17 @@ def bag(n, c, w, v):
 	return res
 
 def show(n, c, w, res):
-	print('×î´ó¼ÛÖµÎª:', res[n][c])
+	print('æœ€å¤§ä»·å€¼ä¸º:', res[n][c])
 	x = [False for i in range(n)]
 	j = c
 	for i in range(1, n + 1):
 		if res[i][j] > res[i - 1][j]:
 			x[i - 1] = True
 			j -= w[i - 1]
-	print('Ñ¡ÔñµÄÎïÆ·Îª:')
+	print('é€‰æ‹©çš„ç‰©å“ä¸º:')
 	for i in range(n):
 		if x[i]:
-			print('µÚ', i, '¸ö,', end='')
+			print('ç¬¬', i, 'ä¸ª,', end='')
 	print('')
 
 if __name__ == '__main__':
@@ -32,3 +32,4 @@ if __name__ == '__main__':
 	v = [6,3,5,4,6]
 	res = bag(n, c, w, v)
 	show(n, c, w, res)
+	
